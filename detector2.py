9bcc7cfbd590d67da5795dbcab7dcc484bfab4ec
Bruno Cruz <cruzzz.1998@live.com.pt>
2024-06-26 09:57:19 +0100

    Commit

--- detector2.py
+++ detector2.py
@@ -2,14 +2,18 @@ import sys, qdarktheme, time, os
 import PyQt6.QtWidgets as Widgets
 import PyQt6.QtCore as Core
 from PyQt6 import QtGui
-from qtwidgets import AnimatedToggle, Toggle
+from pyqt_switch import PyQtSwitch
+
+app = Widgets.QApplication(sys.argv)
 
 app_title = "Detector"
+media_source = 0
+record_on = False
+prints_on = False
 
 class GUI(Widgets.QMainWindow):
     def __init__(self, screen_size):
         super().__init__()
-        qdarktheme.setup_theme(custom_colors={"primary": "#FFFFFF"})
         # Screen and App Sizes
         self.screen_width = screen_size.width()
         self.screen_height = screen_size.height()
@@ -20,10 +24,9 @@ class GUI(Widgets.QMainWindow):
         # INIT WINDOW
         self.setFixedSize(self.app_width, self.app_height)
         self.setWindowTitle(app_title)
-        self.setWindowIcon(QtGui.QIcon("./icons/favicon.ico"))
+        self.setWindowIcon(QtGui.QIcon("./favicon.ico"))
         # MAIN APP LAYOUT
         self.app_layout = Widgets.QVBoxLayout()
-        # self.offset = None
 
         self.choose_harr_layout = Widgets.QHBoxLayout()
 
@@ -39,64 +42,76 @@ class GUI(Widgets.QMainWindow):
         self.choose_source_layout = Widgets.QHBoxLayout()
 
         self.source_image_button = Widgets.QPushButton("Image")
+        self.source_image_button.setStyleSheet('QPushButton { background-color: grey }')
+        self.source_image_button.clicked.connect(lambda: self.toggleSource(0))
         self.choose_source_layout.addWidget(self.source_image_button)
 
-        self.source_image_button = Widgets.QPushButton("Video")
-        self.choose_source_layout.addWidget(self.source_image_button)
+        self.source_video_button = Widgets.QPushButton("Video")
+        self.source_video_button.setStyleSheet('QPushButton { background-color: darkgrey }')
+        self.source_video_button.clicked.connect(lambda: self.toggleSource(1))
+        self.choose_source_layout.addWidget(self.source_video_button)
 
-        self.source_image_button = Widgets.QPushButton("Camera")
-        self.choose_source_layout.addWidget(self.source_image_button)
+        self.source_camera_button = Widgets.QPushButton("Camera")
+        self.source_camera_button.setStyleSheet('QPushButton { background-color: darkgrey }')
+        self.source_camera_button.clicked.connect(lambda: self.toggleSource(2))
+        self.choose_source_layout.addWidget(self.source_camera_button)
 
         self.app_layout.addLayout(self.choose_source_layout)
 
         self.record_detection_layout = Widgets.QHBoxLayout()
 
-        self.toggle_1 = Toggle()
-        self.toggle_2 = AnimatedToggle(
-            checked_color="#FFB000",
-            pulse_checked_color="#44FFB000"
-        )
-        self.record_detection_layout.addWidget(self.toggle_1)
-        self.record_detection_layout.addWidget(self.toggle_2)
+        self.record_switch = Widgets.QPushButton("Record detection", self)
+        self.record_switch.setStyleSheet('QPushButton { background-color: red;}')
+        self.record_switch.clicked.connect(self.toggleRecord)
+        self.record_detection_layout.addWidget(self.record_switch)
+
+        self.print_switch = Widgets.QPushButton("Print detections", self)
+        self.print_switch.setStyleSheet('QPushButton { background-color: red;}')
+        self.print_switch.clicked.connect(self.togglePrints)
+        self.record_detection_layout.addWidget(self.print_switch)
 
         self.app_layout.addLayout(self.record_detection_layout)
+
+        self.start_button = Widgets.QPushButton("Start", self)
+        self.app_layout.addWidget(self.start_button)
 
         # RUN
         self.main_window = Widgets.QWidget()
         self.main_window.setLayout(self.app_layout)
         self.setCentralWidget(self.main_window)
+        self.show()
 
-        self.timer = Core.QTimer(self)
-        self.timer.setInterval(100)
-        self.timer.timeout.connect(self.update_ui)
+    def toggleSource(self, source):
+        global media_source
+        media_source = source
+        self.source_image_button.setStyleSheet('QPushButton { background-color: darkgrey }')
+        self.source_video_button.setStyleSheet('QPushButton { background-color: darkgrey }')
+        self.source_camera_button.setStyleSheet('QPushButton { background-color: darkgrey }')
+        if source == 0:
+            self.source_image_button.setStyleSheet('QPushButton { background-color: grey }')
+        elif source == 1:
+            self.source_video_button.setStyleSheet('QPushButton { background-color: grey }')
+        else:
+            self.source_camera_button.setStyleSheet('QPushButton { background-color: grey }')
 
-    def set_position(self):
-        self.timer.stop()
-        pos = self.media_range_slider.value()
-        self.mediaplayer.set_position(pos / self.mediaplayer.get_length())
-        self.timer.start()
+    def toggleRecord(self):
+        global prints_on
+        if prints_on:
+            prints_on = False
+            self.record_switch.setStyleSheet('QPushButton { background-color: green;}')
+        else:
+            prints_on = True
+            self.record_switch.setStyleSheet('QPushButton { background-color: red;}')
 
-    def update_ui(self):
-        """Updates the user interface"""
-
-        # Set the slider's position to its corresponding media position
-        # Note that the setValue function only takes values of type int,
-        # so we must first convert the corresponding media position.
-        media_pos = int(self.mediaplayer.get_position() * self.mediaplayer.get_length())
-        self.media_range_slider.setValue(media_pos)
-
-        # No need to call this function if nothing is played
-        if not self.mediaplayer.is_playing():
-            self.timer.stop()
-
-            # # After the video finished, the play button stills shows "Pause",
-            # # which is not the desired behavior of a media player.
-            # # This fixes that "bug".
-            # if not self.mediaplayer.is_paused:
-            #     self.timer.stop()
+    def togglePrints(self):
+        global record_on
+        if record_on:
+            record_on = False
+            self.print_switch.setStyleSheet('QPushButton { background-color: green;}')
+        else:
+            record_on = True
+            self.print_switch.setStyleSheet('QPushButton { background-color: red;}')
 
 if __name__ == "__main__":
-    app = Widgets.QApplication(sys.argv)
     gui = GUI(screen_size=app.primaryScreen().size())
-    gui.show()
     sys.exit(app.exec())
